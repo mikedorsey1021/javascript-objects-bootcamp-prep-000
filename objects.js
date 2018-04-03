64a71fbc27f2306599a847addbf751058c9d4dc3
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-03 14:31:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = {fredHammond: "glory, to glory"}
 
 function updatePlaylist (playlist, artistName, songTitle){
-  return object.assign({}, playlist, { [artistName]: songTitle})
+  playlist[artistName] = songTitle; 
+  return playlist
 }
